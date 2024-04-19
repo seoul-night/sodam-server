fdfce213192f87b56fd69c2ff9cda0d89798add1
김수영 <kswim8914@gmail.com>
2024-04-19 17:20:19 +0900

    Init: 프로젝트 세팅

---
+++ src/main/java/growthook/org/bamgang/BamgangApplication.java
@@ -0,0 +1,13 @@
+package growthook.org.bamgang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BamgangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BamgangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/growthook/org/bamgang/BamgangApplicationTests.java
@@ -0,0 +1,13 @@
+package growthook.org.bamgang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamgangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
