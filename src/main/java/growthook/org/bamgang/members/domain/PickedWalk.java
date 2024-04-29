ad3f18d91be557fedb07cca2904b62f322aed90d
jaejunlee11 <72538151+jaejunlee11@users.noreply.github.com>
2024-04-29 21:39:31 +0900

    feat: #3-Entity 생성

---
+++ src/main/java/growthook/org/bamgang/members/domain/PickedWalk.java
@@ -0,0 +1,29 @@
+package growthook.org.bamgang.members.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "pickedtrail")
+public class PickedWalk {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+
+    @Id
+    private int trailId;
+
+    private int userId;
+
+    private Date pickedDay;
+
+    private String trailRegion;
+
+
+}
