4c06230644c49c6dae723aab42de2e00992f65fe
jaejunlee11 <72538151+jaejunlee11@users.noreply.github.com>
2024-04-30 21:45:31 +0900

    feat: #20-config 설정 완료

---
+++ src/main/java/growthook/org/bamgang/config/WebmvcConfig.java
@@ -0,0 +1,18 @@
+package growthook.org.bamgang.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebmvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
+                .allowedHeaders("headers")
+                .maxAge(3000);
+    }
+}
