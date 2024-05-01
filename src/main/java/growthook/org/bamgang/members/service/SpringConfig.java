ed4a792116881aa731544aaca578fc5faa969040
jaejunlee11 <72538151+jaejunlee11@users.noreply.github.com>
2024-05-01 13:29:47 +0900

    fix: #15-trail정보 조회를 통한 데이터 저장 구현완료

--- src/main/java/growthook/org/bamgang/members/service/SpringConfig.java
+++ src/main/java/growthook/org/bamgang/members/service/SpringConfig.java
@@ -3,6 +3,7 @@ package growthook.org.bamgang.members.service;
 import growthook.org.bamgang.members.repository.DataFinishedWalkRepository;
 import growthook.org.bamgang.members.repository.DataPickedWalkRepository;
 import growthook.org.bamgang.members.repository.MemberRepository;
+import growthook.org.bamgang.trail.repository.TrailRepository;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -17,15 +18,18 @@ public class SpringConfig {
 
     private final DataPickedWalkRepository dataPickedWalkRepository;
 
+    private final TrailRepository trailRepository;
+
     @Autowired
-    public SpringConfig(MemberRepository memberRepository, DataFinishedWalkRepository finishedWalkRepository, DataPickedWalkRepository dataPickedWalkRepository) {
+    public SpringConfig(MemberRepository memberRepository, DataFinishedWalkRepository finishedWalkRepository, DataPickedWalkRepository dataPickedWalkRepository, TrailRepository trailRepository) {
         this.finishedWalkRepository = finishedWalkRepository;
         this.dataPickedWalkRepository = dataPickedWalkRepository;
         this.memberRepository = memberRepository;
+        this.trailRepository = trailRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository,finishedWalkRepository, dataPickedWalkRepository);
+        return new MemberService(memberRepository,finishedWalkRepository, dataPickedWalkRepository,trailRepository);
     }
 }
