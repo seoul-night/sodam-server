a47c3b175ff27bb48ada1bf514c93cec7702b2b0
김수영 <kswim8914@gmail.com>
2024-05-01 17:42:51 +0900

    feat: #6-TrailController에 getNearTrail() 생성

--- src/main/java/growthook/org/bamgang/trail/controller/TrailController.java
+++ src/main/java/growthook/org/bamgang/trail/controller/TrailController.java
@@ -5,6 +5,8 @@ import growthook.org.bamgang.trail.dto.response.GetTrailResponseDto;
 import growthook.org.bamgang.trail.service.TrailService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/walks")
 public class TrailController {
@@ -12,9 +14,16 @@ public class TrailController {
     public TrailController(TrailService trailService) {
         this.trailService = trailService;
     }
+
+    // 산책로 상세정보 조회
     @GetMapping("/{trailId}")
     public GetTrailResponseDto getTrailById(@PathVariable("trailId") Integer trailId){
         return trailService.getTrailById(trailId);
+    }
+
+    @GetMapping("/near/{latitude}/{longitude}")
+    public List<GetTrailResponseDto> getNearTrail(@PathVariable("latitude") Double latitude, @PathVariable("longitude") Double longitude){
+        return trailService.getNearTrail(latitude, longitude);
     }
 
 }
