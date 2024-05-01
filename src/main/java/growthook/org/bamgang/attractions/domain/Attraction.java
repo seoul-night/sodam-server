9c6779ba10a0058affba75edd6fab052ba22ec72
jaejunlee11 <72538151+jaejunlee11@users.noreply.github.com>
2024-05-01 18:09:45 +0900

    fix: #30-domain수정 완료

--- src/main/java/growthook/org/bamgang/attractions/domain/Attraction.java
+++ src/main/java/growthook/org/bamgang/attractions/domain/Attraction.java
@@ -4,11 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "attraction")
 public class Attraction {
     @Id
